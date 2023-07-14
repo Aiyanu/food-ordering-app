f53fc0a378e3c345f047950b146fb1b3868ce8a1
Iyanuoluwapo Adesanya <aiyanu1.00@gmail.com>
2023-07-14 11:21:07 +0100

    start fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,28 @@
-import Navbar from '@/components/Navbar'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/Footer'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Pizza Restaurant',
-  description: 'Best Pizza shop in town',
-}
+  title: "Pizza Restaurant",
+  description: "Best Pizza shop in town",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <Navbar/>
-      <body className={inter.className}>{children}</body>
-      <Footer/>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
